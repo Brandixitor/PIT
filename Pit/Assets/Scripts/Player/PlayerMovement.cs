da56fd6fc512f4f84f32581932580572c34cf01f
Brandixitor <med.salah.bourouba@gmail.com>
2021-04-23 15:44:32 +0100

    Update

--- Pit/Assets/Scripts/Player/PlayerMovement.cs
+++ Pit/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour
  //Variables   
  public CharacterController controller;
  public float speed = 12f;
+ public float gravity = -9.81f;
+ Vector3 velocity;
 
     
 
@@ -21,6 +23,9 @@ public class PlayerMovement : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
         
     }
 }
--- README.md
+++ README.md
@@ -9,3 +9,4 @@ Also, work on the advanced movement system including crouching and sprinting. </
 
 
 - 23/04/2021 - Finished working the mouse Y Axis. </br>
+Added gravity physics to the player movements. </br>
